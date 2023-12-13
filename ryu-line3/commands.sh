e8fd7068e9bab57f4af2a07d83581122b75e38cf
tryuuu <113238295+tryuuu@users.noreply.github.com>
2023-12-14 01:09:09 +0900

    Delete ryu-line3/commands.sh

--- ryu-line3/commands.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 361452760157.dkr.ecr.ap-northeast-1.amazonaws.com
-docker build -t ryu-ocr-test --platform linux/x86_64 .
-docker tag ryu-ocr-test:latest 361452760157.dkr.ecr.ap-northeast-1.amazonaws.com/ryu-ocr-test:latest
-docker push 361452760157.dkr.ecr.ap-northeast-1.amazonaws.com/ryu-ocr-test:latest
